4df25b8a1e08dc0da404881ca8e6f955cd16754b
niccofil <nikita.ttu@gmail.com>
2021-04-03 12:51:39 +0300

    Countries Table added

--- netflix.py
+++ netflix.py
@@ -12,22 +12,32 @@ cursor.execute("""CREATE TABLE shows (
     type TEXT,
     year INTEGER,
     rating TEXT) """)
-# Create actors table
+# Create people table
 cursor.execute("CREATE TABLE people (id INTEGER PRIMARY KEY, name TEXT)")
-# Link shows table to actors table
+# Create countries table
+cursor.execute("CREATE TABLE countries(id INTEGER PRIMARY KEY, country TEXT)")
+# Link shows table to actors tables
 cursor.execute("""CREATE TABLE shows_actors (
     show_id INTEGER,
     actor_id INTEGER,
     FOREIGN KEY (show_id) REFERENCES shows (id),
-    FOREIGN KEY(actor_id) REFERENCES people(id)) """)
-# Link shows to directors table
+    FOREIGN KEY(actor_id) REFERENCES people(id))""")
+# Link shows to directors tables
 cursor.execute("""CREATE TABLE shows_directors (
     show_id INTEGER,
     director_id INTEGER,
     FOREIGN KEY (show_id) REFERENCES shows (id),
-    FOREIGN KEY(director_id) REFERENCES people(id)) """)
+    FOREIGN KEY(director_id) REFERENCES people (id))""")
+# Link shows to countries tables
+cursor.execute("""CREATE TABLE shows_countries (
+    show_id INTEGER,
+    country_id INTEGER,
+    FOREIGN KEY (show_id) REFERENCES shows (id),
+    FOREIGN KEY(country_id) REFERENCES countries (id))""")
+
 with open("netflix_titles.csv") as file:
-    people = {} 
+    people = {}
+    countries = {} 
     reader = csv.DictReader(file)
     for row in reader:
         shows = [row["title"], row["type"], row["release_year"], row["rating"]]
@@ -40,16 +50,26 @@ with open("netflix_titles.csv") as file:
         directors = row["director"].split(",")
         # List of actors and directors
         names = actors + directors
+        # List of countries
+        ctries = row["country"].split(",")
         # Add names with unique id to people table
         for name in names:
             if name.strip() not in people.keys():
                 cursor.execute("INSERT INTO people (name) VALUES(?)", (name.strip(),))
                 people[name.strip()] = cursor.lastrowid
+        # Add countris with unique id to countries table
+        for country in ctries:
+            if country.strip() not in countries.keys():
+                cursor.execute("INSERT INTO countries(country) VALUES(?)", (country.strip(),))
+                countries[country.strip()] = cursor.lastrowid
         # Add to joint table show_id and actor_id        
         for i in range(len(actors)):
                     cursor.execute("INSERT INTO shows_actors (show_id, actor_id) VALUES(?,?)", (show_id, people[actors[i].strip()]))
         # Add to joint table show_id and director_id  
         for i in range(len(directors)):
                 cursor.execute("INSERT INTO shows_directors (show_id, director_id) VALUES(?,?)", (show_id, people[directors[i].strip()]))
+        # Add to joint table show_id and country_id  
+        for i in range(len(ctries)):
+                cursor.execute("INSERT INTO shows_countries (show_id, country_id) VALUES(?,?)", (show_id, countries[ctries[i].strip()]))
 con.commit()
-con.close()
+con.close() 
